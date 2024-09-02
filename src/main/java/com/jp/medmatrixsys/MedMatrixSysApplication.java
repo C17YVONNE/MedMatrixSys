c90409d9e34d08774965b47fab2819611ef1d755
C17YVONNE <c17yvonne@gmail.com>
2024-09-02 18:01:47 +0900

    ｃ
    ｃ

---
+++ src/main/java/com/jp/medmatrixsys/MedMatrixSysApplication.java
@@ -0,0 +1,13 @@
+package com.jp.medmatrixsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedMatrixSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedMatrixSysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jp/medmatrixsys/MedMatrixSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jp.medmatrixsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedMatrixSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
